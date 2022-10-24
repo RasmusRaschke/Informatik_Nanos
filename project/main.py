be35b7e1bb7110d614d2a9bff2f6ad81c4e80b9e
Rasmus Raschke <rasmus.raschke@gmail.com>
2022-10-24 11:12:45 +0200

    plot and start of newton-raphson

--- project/main.py
+++ project/main.py
@@ -73,7 +73,7 @@ def get_k(v, E):
         k.append((v[i] - E) * 26.27)
     return k
 
-def numerov(u_0, u_1, K, counter):
+def numerov(u_0, u_1, K, counter, x_min, x_max, N):
     """Apply numerov algorithm to solve separated SEQ
 
     Parameters
@@ -86,6 +86,12 @@ def numerov(u_0, u_1, K, counter):
         constant in SQE
     counter : int
         uses counter (=len(v)) to make sure that index range is conserved when using a variable amount of wells
+    x_min : float
+        start of x-axis
+    x_max : float
+        end of x-axis
+    N : int
+        number of steps
 
     Returns
     -------
@@ -94,12 +100,43 @@ def numerov(u_0, u_1, K, counter):
 
     """
     u = [u_0, u_1]
+    h = (x_max - x_min) / N
     for i in range(2, counter):
         u.append((2 * u[i - 1] * (1 - (5 / 12) * h**2 * K) - u[i - 2] * (1 + h**2 / 12 * K)) / (1 + 1 / 12 * h**2 * K))
     return u
 
-def norm():
+def norm(u):
+    """returns normed wave function
+
+        Parameters
+        ----------
+        u : list
+            wave function from numerov algorithm
+
+        Returns
+        -------
+        u_norm : list
+            normed wave function
+
+        """
+    sum_ = 0
+    u_norm = []
+    for i in range(len(u)):
+        sum_ += u[i]**2
+        u_norm[i] = u[i] / np.sqrt(sum)
+    return u_norm
+
+
+def boundary_cond(u_0, K, counter, x_min, x_max, N):
     pass
+    '''
+    u_var = 0
+    u_max = []
+    while True:
+        wave = numerov(u_0, u_var, K, counter, x_min, x_max, N)
+        u_max = u_max.append(wave[-1])
+        u_var = 
+    '''
 ########################################################################################################################
 #Variablen hier
 v_0 = 2
@@ -108,8 +145,10 @@ L = 2
 N = 1000
 x_min = 0
 x_max = 10
-wells = 2
+wells = 5
 E = 1.7
+u_0 = .0
+u_1 = .1
 ########################################################################################################################
 #Berechnung
 print(periodic(v_0, x_0, L, N, x_min, x_max, wells))
