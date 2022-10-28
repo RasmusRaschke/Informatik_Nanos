8a55d64315cc51c4d638da8a1359b3066b61ba8c
Rasmus Raschke <rasmus.raschke@gmail.com>
2022-10-28 14:30:33 +0200

    several changes to main, right now not functional

--- project/main.py
+++ project/main.py
@@ -102,7 +102,7 @@ def numerov(u_0, u_1, K, counter, x_min, x_max, N):
     u = [u_0, u_1]
     h = (x_max - x_min) / N
     for i in range(2, counter):
-        u.append((2 * u[i - 1] * (1 - (5 / 12) * h**2 * K) - u[i - 2] * (1 + h**2 / 12 * K)) / (1 + 1 / 12 * h**2 * K))
+        u.append((2 * u[i - 1] * (1 - (5 / 12) * h**2 * K[i - 1]) - u[i - 2] * (1 + h**2 / 12 * K[i - 2])) / (1 + 1 / 12 * h**2 * K[i]))
     return u
 
 def norm(u):
@@ -123,40 +123,62 @@ def norm(u):
     u_norm = []
     for i in range(len(u)):
         sum_ += u[i]**2
-        u_norm[i] = u[i] / np.sqrt(sum)
+        if sum != 0:
+            u_norm.append(u[i] / np.sqrt(sum_))
+        else:
+            u_norm.append(0)
     return u_norm
 
 
-def boundary_cond(u_0, K, counter, x_min, x_max, N):
-    pass
-    '''
-    u_var = 0
-    u_max = []
-    while True:
-        wave = numerov(u_0, u_var, K, counter, x_min, x_max, N)
-        u_max = u_max.append(wave[-1])
-        u_var = 
-    '''
+def newton_raphson(u_0, u_1, counter, x_min, x_max, N, v, max_bound, energy, accuracy, start):
+    e = energy
+    de = start
+    for i in range(max_bound):
+        K = get_k(v, e)
+        psi_1 = numerov(u_0, u_1, K, counter, x_min, x_max, N)
+        psi_max = psi_1[N-1]
+        K = get_k(v, e + de)
+        psi_2 = numerov(u_0, u_1, K, counter, x_min, x_max, N)
+        psi_max_de = psi_2[N-1]
+        de_new = - psi_max / ((psi_max_de - psi_max) / de)
+        e_new = e + de_new
+        de = de_new
+        control = e_new - e
+        e = e_new
+        if control < accuracy:
+            break
+    return e_new
+
+
 ########################################################################################################################
 #Variablen hier
-v_0 = 2
+v_0 = 5
 x_0 = 1
 L = 2
 N = 1000
 x_min = 0
 x_max = 10
 wells = 5
-E = 1.7
+E = 0.1
 u_0 = .0
-u_1 = .1
+u_1 = .01
+max_bound = 1000000
+accuracy = 0.01
+start = 0.1
 ########################################################################################################################
 #Berechnung
-print(periodic(v_0, x_0, L, N, x_min, x_max, wells))
+x, v, counter = periodic(v_0, x_0, L, N, x_min, x_max, wells)
+eigen = newton_raphson(u_0, u_1, counter, x_min, x_max, N, v, max_bound, E, accuracy, start)
+K = get_k(v, eigen)
+psi = numerov(u_0, u_1, K, counter, x_min, x_max, N)
+print(psi)
+psi_norm = norm(psi)
+print(eigen)
 ########################################################################################################################
 #Plot
 fig, ax = plt.subplots()
-x, y, counter = periodic(v_0, x_0, L, N, x_min, x_max, wells)
-ax.plot(x, y)
-ax.plot(x, get_k(y, -E))
+ax.plot(x, v)
+ax.plot(x, get_k(v, -E))
+ax.plot(x, psi_norm)
 plt.show()
 
